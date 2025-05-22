3de9d4915d232b7e606e7d565e093395845d0da9
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2025-05-22 14:40:18 -0700

    Add minimal release configuration.
    - Add minimal CMake configuration to build and test the library;
    - Add separate pyproject.toml config files to build runtime-only and
      development Python wheels with headers for downstream packages;
    - Add Github CI scripts to run the tests on major platforms;
    - Add gitignore file;
    - Add moduleDeps.cpp back to explicitly register Python module dependencies;
    - Reorganize Python and DSO test plugins for clarity, and run test discovery
      with pytest-cmake;
    - Use the fully-qualified pxr::PlugNotice::DidRegisterPlugins identifier when
      registering test notices from Python;
    - Generate plugin LibraryPath using CMake's TARGET_FILE expression;
    - Use fully qualified types in plugin metadata to ensure serialized
      identifiers match registry entries.

---
+++ src/pxr/plug/moduleDeps.cpp
@@ -0,0 +1,30 @@
+// Copyright 2016 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+////////////////////////////////////////////////////////////////////////
+
+#include <pxr/tf/registryManager.h>
+#include <pxr/tf/scriptModuleLoader.h>
+#include <pxr/tf/token.h>
+
+#include <vector>
+
+namespace pxr {
+
+TF_REGISTRY_FUNCTION(pxr::TfScriptModuleLoader) {
+    // List of direct dependencies for this library.
+    const std::vector<TfToken> reqs = {
+        TfToken("arch"),
+        TfToken("tf"),
+        TfToken("js"),
+        TfToken("trace"),
+        TfToken("work")
+    };
+    TfScriptModuleLoader::GetInstance().
+        RegisterLibrary(TfToken("plug"), TfToken("pxr.Plug"), reqs);
+}
+
+}  // namespace pxr
